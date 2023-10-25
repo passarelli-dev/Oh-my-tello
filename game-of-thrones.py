905894da2bf9783d2517ea48dfaba286d1c9a468
Alessandro Passarelli <alessandro@passarelli.dev>
2023-10-25 16:57:47 +0200

    0.2 Aggiunti Controlli
    Aumenta la Frequenza di Campionamento: Riduci l'intervallo di tempo tra i chunk di audio che vengono elaborati. Ciò potrebbe consentire una risposta più rapida ai cambiamenti nelle note.
    
    Ottimizza le Azioni del Drone: Valuta se le azioni del drone possono essere ottimizzate o semplificate. Ad esempio, potresti ridurre la distanza o la durata di alcune azioni in modo che il drone risponda più rapidamente.
    
    Implementa un Buffer di Comandi: Piuttosto che eseguire azioni direttamente sulla base di ogni singola nota, potresti accumulare le note rilevate in un buffer e poi interpretarle in base a sequenze o modelli più lunghi. In questo modo, puoi bilanciare il riconoscimento rapido delle note con azioni più ponderate e meno frenetiche.
    
    Aggiungi Logica di Sincronizzazione: Introduci una logica di sincronizzazione che tiene conto del tempo necessario per il drone per eseguire le azioni. Ad esempio, se una nota richiede un movimento lungo, puoi pianificare la successiva nota in modo che sia in sintonia con la fine del movimento corrente.
    
    Aggiungi Limiti di Sicurezza: Implementa limiti di sicurezza che impediscono al drone di eseguire azioni rischiose o eccessivamente rapide.

--- game-of-thrones.py
+++ game-of-thrones.py
@@ -2,6 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import play
 import math
 from djitellopy import TelloSwarm
+import time
 
 def frequency_to_note(frequency):
     # Mappa le frequenze alle note
@@ -28,38 +29,50 @@ threshold_frequency = 440  # Frequenza approssimativa della nota A in Hz
 swarm.connect()
 swarm.takeoff()
 
+# Definisci un intervallo di tempo tra i chunk di audio (in secondi)
+chunk_interval = 0.5
+
+# Buffer per accumulare le note rilevate
+note_buffer = []
+
 for i, chunk in enumerate(song[::1000]):
     if chunk.max >= threshold_frequency:
         note = frequency_to_note(chunk.max)
         print(f"Nota {note} rilevata nel chunk {i}")
 
-        # Esempi di azioni dello sciame in base alla nota
-        if 'C' in note:
-            swarm.move_up(30)
-        elif 'C#' in note:
-            swarm.move_left(30)
-        elif 'D' in note:
-            swarm.move_right(30)
-        elif 'D#' in note:
-            swarm.move_forward(30)
-        elif 'E' in note:
-            swarm.move_back(30)
-        elif 'F' in note:
-            swarm.rotate_counter_clockwise(30)
-        elif 'F#' in note:
-            swarm.rotate_clockwise(30)
-        elif 'G' in note:
-            swarm.flip_forward()
-        elif 'G#' in note:
-            swarm.flip_backward()
-        elif 'A' in note:
-            swarm.flip_left()
-        elif 'A#' in note:
-            swarm.flip_right()
-        elif 'B' in note:
-            swarm.land()
+        # Aggiungi la nota al buffer
+        note_buffer.append(note)
 
-        play(chunk)
+# Processa le note nel buffer
+for note in note_buffer:
+    # Esempi di azioni dello sciame in base alla nota
+    if 'C' in note:
+        swarm.move_up(30)
+    elif 'C#' in note:
+        swarm.move_left(30)
+    elif 'D' in note:
+        swarm.move_right(30)
+    elif 'D#' in note:
+        swarm.move_forward(30)
+    elif 'E' in note:
+        swarm.move_back(30)
+    elif 'F' in note:
+        swarm.rotate_counter_clockwise(30)
+    elif 'F#' in note:
+        swarm.rotate_clockwise(30)
+    elif 'G' in note:
+        swarm.flip_forward()
+    elif 'G#' in note:
+        swarm.flip_backward()
+    elif 'A' in note:
+        swarm.flip_left()
+    elif 'A#' in note:
+        swarm.flip_right()
+    elif 'B' in note:
+        swarm.land()
+
+    # Pausa tra le azioni
+    time.sleep(chunk_interval)
 
 # Atterra lo sciame alla fine
 swarm.land()
